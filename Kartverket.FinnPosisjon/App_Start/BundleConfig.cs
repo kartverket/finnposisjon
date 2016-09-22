863023f541b175239df1ec670a078f420b1b12b5
Benjamin Dehli <benjamin@arkitektum.no>
2016-09-22 12:47:23 +0200

    Add and include bundles for scripts and stylesheets

--- Kartverket.FinnPosisjon/App_Start/BundleConfig.cs
+++ Kartverket.FinnPosisjon/App_Start/BundleConfig.cs
@@ -8,6 +8,13 @@ namespace Kartverket.FinnPosisjon
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/bundles/styles").Include(
+                "~/Content/assets/css/main.min.css"
+            ));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+               "~/Content/assets/js/main.min.js"
+           ));
         }
     }
 }

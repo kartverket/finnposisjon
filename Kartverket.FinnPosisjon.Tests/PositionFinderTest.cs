ee6a6c07b9df5911f1be084486db040994d4b8d1
Jørgen Tellnes <jorgen@arkitektum.no>
2016-09-27 16:40:01 +0200

    Setup initial boundary handling in PositionFinder

--- Kartverket.FinnPosisjon.Tests/PositionFinderTest.cs
+++ Kartverket.FinnPosisjon.Tests/PositionFinderTest.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
+using Kartverket.FinnPosisjon.Models;
 using Kartverket.FinnPosisjon.Services;
 using Xunit;
 
@@ -7,16 +9,136 @@ namespace Kartverket.FinnPosisjon.Tests
 {
     public class PositionFinderTest
     {
-        [Fact (Skip = "PositionFinder.Find not yet implemented")]
+        
+        [Fact(Skip = "PositionFinder.Find not yet implemented")]
         public void ShouldFindPositionWithCoordSysEu89UtmZone33()
         {
             var userInputCoordinates = new[] {"163067.449", "6601114.654"};
 
-            var possiblePositions = new PositionFinder().Find(userInputCoordinates);
+            var positionFinder = InitPositionFinder();
+
+            var possiblePositions = positionFinder.Find(userInputCoordinates);
 
             var position = possiblePositions.First();
 
             position.CoordinateSystem.Should().Be("EU89, UTM-sone 33");
+        }
+
+        [Fact(Skip = "Test is unfinished")]
+        public void ShouldFindOnePositionWithinBounderies()
+        {
+            var userInputCoordinates = new[] { "45", "15" };
+
+            var positionFinder = InitPositionFinder();
+
+            var possiblePositions = positionFinder.Find(userInputCoordinates);
+
+            possiblePositions.Count.Should().Be(1);
+        }
+
+        private static PositionFinder InitPositionFinder()
+        {
+            return new PositionFinder
+            {
+                SupportedCoordinateSystems = new List<CoordinateSystem>
+                {
+                    new CoordinateSystem
+                    {
+                        // TODO: Setup for EUREF89
+                        Name = "System 1",
+                        BoundaryBoxes = new List<BoundaryBox>
+                        {
+                            new BoundaryBox
+                            {
+                                MinEast = 30,
+                                MaxEast = 60,
+                                MinNorth = 10,
+                                MaxNorth = 20
+                            },
+                            new BoundaryBox
+                            {
+                                MinEast = 30,
+                                MaxEast = 60,
+                                MinNorth = 10,
+                                MaxNorth = 20
+                            },
+                            new BoundaryBox
+                            {
+                                MinEast = 30,
+                                MaxEast = 60,
+                                MinNorth = 10,
+                                MaxNorth = 20
+                            }
+                        }
+                    },
+                    new CoordinateSystem
+                    {
+                        // TODO: Setup for ITRF/WGS84
+                        Name = "System 2",
+                        BoundaryBoxes = new List<BoundaryBox>
+                        {
+                            new BoundaryBox
+                            {
+                                MinEast = 30,
+                                MaxEast = 60,
+                                MinNorth = 10,
+                                MaxNorth = 20
+                            },
+                            new BoundaryBox
+                            {
+                                MinEast = 30,
+                                MaxEast = 60,
+                                MinNorth = 10,
+                                MaxNorth = 20
+                            },
+                            new BoundaryBox
+                            {
+                                MinEast = 30,
+                                MaxEast = 60,
+                                MinNorth = 10,
+                                MaxNorth = 20
+                            }
+                        }
+                    },
+                    new CoordinateSystem
+                    {
+                        // TODO: Setup for NGO1948
+                        Name = "System 3",
+                        BoundaryBoxes = new List<BoundaryBox>
+                        {
+                            new BoundaryBox
+                            {
+                                MinEast = 30,
+                                MaxEast = 60,
+                                MinNorth = 10,
+                                MaxNorth = 20
+                            },
+                            new BoundaryBox
+                            {
+                                MinEast = 30,
+                                MaxEast = 60,
+                                MinNorth = 10,
+                                MaxNorth = 20
+                            }
+                        }
+                    },
+                    new CoordinateSystem
+                    {
+                        // TODO: Setup for 'Oslo kommunes lokale koordinatsystem'
+                        Name = "System 4",
+                        BoundaryBoxes = new List<BoundaryBox>
+                        {
+                            new BoundaryBox
+                            {
+                                MinEast = 30,
+                                MaxEast = 60,
+                                MinNorth = 10,
+                                MaxNorth = 20
+                            }
+                        }
+                    }
+                }
+            };
         }
     }
 }
--- Kartverket.FinnPosisjon/Services/PositionFinder.cs
+++ Kartverket.FinnPosisjon/Services/PositionFinder.cs
@@ -6,18 +6,41 @@ namespace Kartverket.FinnPosisjon.Services
 {
     public class PositionFinder
     {
+        public List<CoordinateSystem> SupportedCoordinateSystems { get; set; }
+
         public List<Position> Find(string[] coordinateSet)
         {
             var possibleCoordinates = GetPossibleCoordinates(coordinateSet);
-            
-            // TODO: Create positions from relevant coordinates ...
+
+            // Remove any coordinates out of bounds:
+            foreach (var coordinates in possibleCoordinates)
+                if (IsOutOfBounds(coordinates))
+                    possibleCoordinates.Remove(coordinates);
 
             var positions = new List<Position>();
-            
+
+            // Return the empty list of positions if no coordinates could be made from the user input
+            // or if no coordinates were within the bounds for any of the coordinate systems. Sad situation ...
+            if (possibleCoordinates.Count == 0)
+                return positions;
+
+            // TODO: Try create positions from the remaining possible coordinates ...
+
             return positions;
         }
 
-        private static IEnumerable<Coordinates> GetPossibleCoordinates(IReadOnlyList<string> coordinateSet)
+        /**
+         * Returns true if the coordinates is out of boundary for
+         * every one of the supported coordinatesystems.
+         */
+
+        private bool IsOutOfBounds(Coordinates coordinates)
+        {
+            return SupportedCoordinateSystems.TrueForAll(
+                coordinateSystem => coordinateSystem.IsOutOfBounds(coordinates));
+        }
+
+        private static List<Coordinates> GetPossibleCoordinates(IReadOnlyList<string> coordinateSet)
         {
             var firstNumber = Convert.ToSingle(coordinateSet[0]);
             var secondNumber = Convert.ToSingle(coordinateSet[1]);

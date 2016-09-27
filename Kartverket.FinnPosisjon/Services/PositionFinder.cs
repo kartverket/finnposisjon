4229cd9a4ed1454c97cf69d9ab6c88d569bd1400
Jørgen Tellnes <jorgen@arkitektum.no>
2016-09-27 14:16:19 +0200

    Add method for converting user-coordinates to system-coordinates

--- Kartverket.FinnPosisjon/Services/PositionFinder.cs
+++ Kartverket.FinnPosisjon/Services/PositionFinder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Kartverket.FinnPosisjon.Models;
 
 namespace Kartverket.FinnPosisjon.Services
@@ -7,9 +8,49 @@ namespace Kartverket.FinnPosisjon.Services
     {
         public List<Position> Find(string[] coordinateSet)
         {
-            // TODO: Find possible positions from coordinate set.
+            var possibleCoordinates = GetPossibleCoordinates(coordinateSet);
+            
+            // TODO: Create positions from relevant coordinates ...
 
-            return new List<Position>();
+            var positions = new List<Position>();
+            
+            return positions;
+        }
+
+        private static IEnumerable<Coordinates> GetPossibleCoordinates(IReadOnlyList<string> coordinateSet)
+        {
+            var firstNumber = Convert.ToSingle(coordinateSet[0]);
+            var secondNumber = Convert.ToSingle(coordinateSet[1]);
+
+            // TODO: Handle possible Z-value
+
+            return new List<Coordinates>(new[]
+            {
+                new Coordinates
+                {
+                    // Normal order
+                    East = firstNumber,
+                    North = secondNumber
+                },
+                new Coordinates
+                {
+                    // Swapped order
+                    East = secondNumber,
+                    North = firstNumber
+                },
+                new Coordinates
+                {
+                    // Normal order, negative east
+                    East = 1 - firstNumber,
+                    North = secondNumber
+                },
+                new Coordinates
+                {
+                    // Swapped order, negative east
+                    East = 1 - secondNumber,
+                    North = firstNumber
+                }
+            });
         }
     }
 }

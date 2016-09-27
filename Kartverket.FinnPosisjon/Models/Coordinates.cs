de6f751bc9e8aa7a1437c08d46de63b2fbfc9a6a
Jørgen Tellnes <jorgen@arkitektum.no>
2016-09-27 09:39:32 +0200

    Rename coordinate fields

--- Kartverket.FinnPosisjon/Models/Coordinates.cs
+++ Kartverket.FinnPosisjon/Models/Coordinates.cs
@@ -2,8 +2,8 @@
 {
     public class Coordinates
     {
-        public float Latitude { get; set; }
-        public float Longitude { get; set; }
+        public float East { get; set; }
+        public float North { get; set; }
         public float Elevation { get; set; }
     }
 }

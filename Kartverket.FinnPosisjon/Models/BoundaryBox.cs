ad2af76da315b6c07d90eec828917baf90277f28
Jørgen Tellnes <jorgen@arkitektum.no>
2016-09-27 16:08:55 +0200

    Add models CoordinateSystem and BoundaryBox

---
+++ Kartverket.FinnPosisjon/Models/BoundaryBox.cs
@@ -0,0 +1,10 @@
+﻿namespace Kartverket.FinnPosisjon.Models
+{
+    public class BoundaryBox
+    {
+        public float MinEast { get; set; }
+        public float MaxEast { get; set; }
+        public float MinNorth { get; set; }
+        public float MaxNorth { get; set; }
+    }
+}
---
+++ Kartverket.FinnPosisjon/Models/CoordinateSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Kartverket.FinnPosisjon.Models
+{
+    public class CoordinateSystem
+    {
+        public string Name { get; set; }
+
+        public List<BoundaryBox> BoundaryBoxes { get; set; }
+
+        public bool IsOutOfBounds(Coordinates coordinates)
+        {
+            return BoundaryBoxes.TrueForAll(boundaryBox =>
+                (coordinates.East < boundaryBox.MinEast) || (coordinates.East > boundaryBox.MaxEast) ||
+                (coordinates.North < boundaryBox.MinNorth) || (coordinates.North > boundaryBox.MaxNorth));
+        }
+    }
+}

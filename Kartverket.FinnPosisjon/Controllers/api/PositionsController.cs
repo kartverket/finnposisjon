347895bc3b3aee6e2350e9fc4e6baca251fb8528
Jørgen Tellnes <jorgen@arkitektum.no>
2016-09-28 12:56:53 +0200

    Add API framework and Positions API controller

---
+++ Kartverket.FinnPosisjon/Controllers/api/PositionsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace Kartverket.FinnPosisjon.Controllers.api
+{
+    public class PositionsController : ApiController
+    {
+        public JsonResult<string> Get(string firstInputValue, string secondInputValue, string thirdInputValue)
+        {
+            return Json("Test");
+        }
+    }
+}

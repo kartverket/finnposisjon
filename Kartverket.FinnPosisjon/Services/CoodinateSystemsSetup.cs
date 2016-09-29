49298989d3feda0f5b04f708064051d2a09a5f0c
Jørgen Tellnes <jorgen@arkitektum.no>
2016-09-29 16:30:52 +0200

    Setup CoordinateSystems with actual numbers and names

--- Kartverket.FinnPosisjon/Services/CoodinateSystemsSetup.cs
+++ Kartverket.FinnPosisjon/Services/CoodinateSystemsSetup.cs
@@ -11,96 +11,83 @@ namespace Kartverket.FinnPosisjon.Services
             {
                 new CoordinateSystem
                 {
-                    // TODO: Setup for EUREF89
-                    Name = "System 1",
+                    Name = "EUREF89",
                     BoundaryBoxes = new List<BoundaryBox>
                     {
+                        // UTM zone 32
                         new BoundaryBox
                         {
-                            MinEast = 30,
-                            MaxEast = 60,
-                            MinNorth = 10,
-                            MaxNorth = 20
+                            MinEast = 229614.1053,
+                            MaxEast = 751898.5673,
+                            MinNorth = 6401682.026,
+                            MaxNorth = 7231445.376
                         },
+                        // UTM zone 33
                         new BoundaryBox
                         {
-                            MinEast = 30,
-                            MaxEast = 60,
-                            MinNorth = 10,
-                            MaxNorth = 20
+                            MinEast = 288889.7639,
+                            MaxEast = 804809.936,
+                            MinNorth = 7231445.376,
+                            MaxNorth = 7866186.306
                         },
+                        // UTM zone 35
                         new BoundaryBox
                         {
-                            MinEast = 30,
-                            MaxEast = 60,
-                            MinNorth = 10,
-                            MaxNorth = 20
+                            MinEast = 253177.3653,
+                            MaxEast = 683621.7167,
+                            MinNorth = 7866186.306,
+                            MaxNorth = 7924929.221
                         }
                     }
                 },
                 new CoordinateSystem
                 {
-                    // TODO: Setup for ITRF/WGS84
-                    Name = "System 2",
+                    Name = "WGS84",
                     BoundaryBoxes = new List<BoundaryBox>
                     {
                         new BoundaryBox
                         {
-                            MinEast = 30,
-                            MaxEast = 60,
-                            MinNorth = 10,
-                            MaxNorth = 20
-                        },
-                        new BoundaryBox
-                        {
-                            MinEast = 30,
-                            MaxEast = 60,
-                            MinNorth = 10,
-                            MaxNorth = 20
-                        },
-                        new BoundaryBox
-                        {
-                            MinEast = 30,
-                            MaxEast = 60,
-                            MinNorth = 10,
-                            MaxNorth = 20
+                            MinEast = 3.844925191,
+                            MaxEast = 31.95907717,
+                            MinNorth = 57.69458922,
+                            MaxNorth = 71.45477563
                         }
                     }
                 },
                 new CoordinateSystem
                 {
-                    // TODO: Setup for NGO1948
-                    Name = "System 3",
+                    Name = "NGO1948",
                     BoundaryBoxes = new List<BoundaryBox>
                     {
+                        // Axis 1-4
                         new BoundaryBox
                         {
-                            MinEast = 30,
-                            MaxEast = 60,
-                            MinNorth = 10,
-                            MaxNorth = 20
+                            MinEast = -368207.9294,
+                            MaxEast = 172305.8,
+                            MinNorth = -28995.15926,
+                            MaxNorth = 808453.3338
                         },
+                        // Axis 5-8
                         new BoundaryBox
                         {
-                            MinEast = 30,
-                            MaxEast = 60,
-                            MinNorth = 10,
-                            MaxNorth = 20
+                            MinEast = -312424.3471,
+                            MaxEast = 410629.5171,
+                            MinNorth = 1507978.752,
+                            MaxNorth = 808453.3338
                         }
                     }
                 },
                 new CoordinateSystem
                 {
-                    // TODO: Setup for 'Oslo kommunes lokale koordinatsystem'
-                    Name = "System 4",
+                    Name = "Oslo kommune",
                     BoundaryBoxes = new List<BoundaryBox>
                     {
                         new BoundaryBox
                         {
-                            MinEast = 30,
-                            MaxEast = 60,
-                            MinNorth = 10,
-                            MaxNorth = 20
+                            MinEast = -13231.52378,
+                            MaxEast = 13557.59229,
+                            MinNorth = -11742.49708,
+                            MaxNorth = 25100.80578
                         }
                     }
                 }

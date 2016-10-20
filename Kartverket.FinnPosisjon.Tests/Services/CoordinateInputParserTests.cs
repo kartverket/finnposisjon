651d29a3693535d0f281f3c1cc608bc6bdadc2b0
Jørgen Tellnes <jorgen@arkitektum.no>
2016-10-20 10:43:42 +0200

    Add unit test for coordinate input parser

---
+++ Kartverket.FinnPosisjon.Tests/Services/CoordinateInputParserTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Kartverket.FinnPosisjon.Models;
+using Kartverket.FinnPosisjon.Services;
+using Xunit;
+
+namespace Kartverket.FinnPosisjon.Tests.Services
+{
+    public class CoordinateInputParserTests
+    {
+        [Fact]
+        public void GetCoordinatesTest()
+        {
+            CoordinateInputParser.GetCoordinates("50", "50").Count.Should().Be(6);
+
+            CoordinateInputParser.GetCoordinates("1", "1", "1").Count.Should().Be(1);
+        }
+    }
+}

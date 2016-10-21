5a3995ea20977b68f5e9a499d111b4c420121382
Benjamin Dehli <benjamin@arkitektum.no>
2016-10-21 16:18:59 +0200

    Adjust styling and scripts for container elements

--- Kartverket.FinnPosisjon/Content/src/js/app.js
+++ Kartverket.FinnPosisjon/Content/src/js/app.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
     $("#find-position").click(function () {
-        $("#loading-animation").addClass("active");
+        $("#loading-animation, #results").addClass("active");
     });
 });

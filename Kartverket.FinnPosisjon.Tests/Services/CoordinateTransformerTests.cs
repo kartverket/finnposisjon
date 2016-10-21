8b7aad8e2821c62a562d2220699abe9be83c9ad0
Jørgen Tellnes <jorgen@arkitektum.no>
2016-10-21 16:05:58 +0200

    Finish test

--- Kartverket.FinnPosisjon.Tests/Services/CoordinateTransformerTests.cs
+++ Kartverket.FinnPosisjon.Tests/Services/CoordinateTransformerTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Globalization;
+using FluentAssertions;
 using Kartverket.FinnPosisjon.Models;
 using Kartverket.FinnPosisjon.Services;
 using Xunit;
@@ -7,83 +8,97 @@ namespace Kartverket.FinnPosisjon.Tests.Services
 {
     public class CoordinateTransformerTests
     {
-        [Fact (Skip = "Higher precision on result than expected result")]
+        [Fact]
         public void TransformTest()
         {
-            Coordinates coordinates;
+            Coordinates originalCoordinates;
             Coordinates transformedCoordinates;
 
             // UTM Zone 32
-            coordinates = new Coordinates { X = 503337.18, Y = 6586036.109 };
-            transformedCoordinates = CoordinateTransformer.Transform(coordinates, 22, 84);
-            transformedCoordinates.X.Should().Be(9.0587915666882122);
-            transformedCoordinates.Y.Should().Be(59.412940371063314);
+            originalCoordinates = new Coordinates {X = 503337.18, Y = 6586036.109};
+            transformedCoordinates = CoordinateTransformer.Transform(originalCoordinates, 22, 84);
+            ClipDecimals(transformedCoordinates.X, 5).Should().Be(9.058);
+            ClipDecimals(transformedCoordinates.Y, 6).Should().Be(59.412);
 
             // UTM Zone 33
-            coordinates = new Coordinates { X = 363414.116, Y = 7121778.269 };
-            transformedCoordinates = CoordinateTransformer.Transform(coordinates, 23, 84);
-            transformedCoordinates.X.Should().Be(12.187198250835472);
-            transformedCoordinates.Y.Should().Be(64.195136674486321);
+            originalCoordinates = new Coordinates {X = 363414.116, Y = 7121778.269};
+            transformedCoordinates = CoordinateTransformer.Transform(originalCoordinates, 23, 84);
+            ClipDecimals(transformedCoordinates.X, 6).Should().Be(12.187);
+            ClipDecimals(transformedCoordinates.Y, 6).Should().Be(64.195);
 
             // UTM Zone 35
-            coordinates = new Coordinates { X = 551933.623, Y = 7826965.416 };
-            transformedCoordinates = CoordinateTransformer.Transform(coordinates, 25, 84);
-            transformedCoordinates.X.Should().Be(28.397021288291612);
-            transformedCoordinates.Y.Should().Be(70.542457224789374);
+            originalCoordinates = new Coordinates {X = 551933.623, Y = 7826965.416};
+            transformedCoordinates = CoordinateTransformer.Transform(originalCoordinates, 25, 84);
+            ClipDecimals(transformedCoordinates.X, 6).Should().Be(28.397);
+            ClipDecimals(transformedCoordinates.Y, 6).Should().Be(70.542);
 
             // NGO48 Axis 1
-            coordinates = new Coordinates { X = 48510.86, Y = 9411.102 };
-            transformedCoordinates = CoordinateTransformer.Transform(coordinates, 1, 84);
-            transformedCoordinates.X.Should().Be(6.87429257348019);
-            transformedCoordinates.Y.Should().Be(58.0826947005639);
+            originalCoordinates = new Coordinates {X = 48510.86, Y = 9411.102};
+            transformedCoordinates = CoordinateTransformer.Transform(originalCoordinates, 1, 84);
+            ClipDecimals(transformedCoordinates.X, 5).Should().Be(6.874);
+            ClipDecimals(transformedCoordinates.Y, 6).Should().Be(58.082);
 
             // NGO48 Axis 2
-            coordinates = new Coordinates { X = 4682.041, Y = 116386.281 };
-            transformedCoordinates = CoordinateTransformer.Transform(coordinates, 2, 84);
-            transformedCoordinates.X.Should().Be(8.46660952588736);
-            transformedCoordinates.Y.Should().Be(59.0457617672893);
+            originalCoordinates = new Coordinates {X = 4682.041, Y = 116386.281};
+            transformedCoordinates = CoordinateTransformer.Transform(originalCoordinates, 2, 84);
+            ClipDecimals(transformedCoordinates.X, 5).Should().Be(8.466);
+            ClipDecimals(transformedCoordinates.Y, 6).Should().Be(59.045);
 
             // NGO48 Axis 3
-            coordinates = new Coordinates { X = 3543.431, Y = 208365.517 };
-            transformedCoordinates = CoordinateTransformer.Transform(coordinates, 3, 84);
-            transformedCoordinates.X.Should().Be(10.7813590642744);
-            transformedCoordinates.Y.Should().Be(59.8714984303773);
+            originalCoordinates = new Coordinates {X = 3543.431, Y = 208365.517};
+            transformedCoordinates = CoordinateTransformer.Transform(originalCoordinates, 3, 84);
+            ClipDecimals(transformedCoordinates.X, 6).Should().Be(10.781);
+            ClipDecimals(transformedCoordinates.Y, 6).Should().Be(59.871);
 
             // NGO48 Axis 4
-            coordinates = new Coordinates { X = -6621688, Y = 680359.286 };
-            transformedCoordinates = CoordinateTransformer.Transform(coordinates, 4, 84);
-            transformedCoordinates.X.Should().Be(13.0814176645284);
-            transformedCoordinates.Y.Should().Be(64.1067615418734);
+            originalCoordinates = new Coordinates {X = -6621.688, Y = 680359.286};
+            transformedCoordinates = CoordinateTransformer.Transform(originalCoordinates, 4, 84);
+            ClipDecimals(transformedCoordinates.X, 6).Should().Be(13.081);
+            ClipDecimals(transformedCoordinates.Y, 6).Should().Be(64.106);
 
             // NGO48 Axis 5
-            coordinates = new Coordinates { X = -65023.284, Y = 982844.292 };
-            transformedCoordinates = CoordinateTransformer.Transform(coordinates, 5, 84);
-            transformedCoordinates.X.Should().Be(15.4036446947715);
-            transformedCoordinates.Y.Should().Be(66.8128663807752);
-            
+            originalCoordinates = new Coordinates {X = -65023.284, Y = 982844.292};
+            transformedCoordinates = CoordinateTransformer.Transform(originalCoordinates, 5, 84);
+            ClipDecimals(transformedCoordinates.X, 6).Should().Be(15.403);
+            ClipDecimals(transformedCoordinates.Y, 6).Should().Be(66.812);
+
             // NGO48 Axis 6
-            coordinates = new Coordinates { X = -47439.511, Y = 1201409.738 };
-            transformedCoordinates = CoordinateTransformer.Transform(coordinates, 6, 84);
-            transformedCoordinates.X.Should().Be(19.7083996599197);
-            transformedCoordinates.Y.Should().Be(68.7755579116143);
+            originalCoordinates = new Coordinates {X = -47439.511, Y = 1201409.738};
+            transformedCoordinates = CoordinateTransformer.Transform(originalCoordinates, 6, 84);
+            ClipDecimals(transformedCoordinates.X, 6).Should().Be(19.708);
+            ClipDecimals(transformedCoordinates.Y, 6).Should().Be(68.775);
 
             // NGO48 Axis 7
-            coordinates = new Coordinates { X = -39261.983, Y = 1391910.869 };
-            transformedCoordinates = CoordinateTransformer.Transform(coordinates, 7, 84);
-            transformedCoordinates.X.Should().Be(23.828597925059);
-            transformedCoordinates.Y.Should().Be(70.4843915636866);
+            originalCoordinates = new Coordinates {X = -39261.983, Y = 1391910.869};
+            transformedCoordinates = CoordinateTransformer.Transform(originalCoordinates, 7, 84);
+            ClipDecimals(transformedCoordinates.X, 6).Should().Be(23.828);
+            ClipDecimals(transformedCoordinates.Y, 6).Should().Be(70.484);
 
             // NGO48 Axis 8
-            coordinates = new Coordinates { X = 12303.924, Y = 1348932.315 };
-            transformedCoordinates = CoordinateTransformer.Transform(coordinates, 8, 84);
-            transformedCoordinates.X.Should().Be(29.3715175096558);
-            transformedCoordinates.Y.Should().Be(70.1021355533797);
+            originalCoordinates = new Coordinates {X = 12303.924, Y = 1348932.315};
+            transformedCoordinates = CoordinateTransformer.Transform(originalCoordinates, 8, 84);
+            ClipDecimals(transformedCoordinates.X, 6).Should().Be(29.371);
+            ClipDecimals(transformedCoordinates.Y, 6).Should().Be(70.102);
 
             // WGS84
-            coordinates = new Coordinates { X = 10.753, Y = 59.905 };
-            transformedCoordinates = CoordinateTransformer.Transform(coordinates, 84, 84);
-            transformedCoordinates.X.Should().Be(10.753);
-            transformedCoordinates.Y.Should().Be(59.905);
+            originalCoordinates = new Coordinates {X = 10.753, Y = 59.905};
+            transformedCoordinates = CoordinateTransformer.Transform(originalCoordinates, 84, 84);
+            ClipDecimals(transformedCoordinates.X, 6).Should().Be(10.753);
+            ClipDecimals(transformedCoordinates.Y, 6).Should().Be(59.905);
+
+
+            // Oslo local coordinate system
+            /*
+            coordinates = new Coordinates { X = 1936.87, Y = -597.994 };
+            transformedCoordinates = CoordinateTransformer.Transform(coordinates, 101, 84);
+            ClipDecimals(transformedCoordinates.X, ).Should().Be();
+            ClipDecimals(transformedCoordinates.Y, ).Should().Be();
+            */
+        }
+        private static double ClipDecimals(double coordinate, int length)
+        {
+            return double.Parse(coordinate.ToString(CultureInfo.InvariantCulture).Substring(0, length),
+                CultureInfo.InvariantCulture);
         }
     }
 }

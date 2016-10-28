5a45832fe94c51356d75fcfe711bda4d0c9be75b
Jørgen Tellnes <jorgen@arkitektum.no>
2016-10-28 10:02:59 +0200

    Add identifier to position model

--- Kartverket.FinnPosisjon/Models/Position.cs
+++ Kartverket.FinnPosisjon/Models/Position.cs
@@ -2,6 +2,7 @@
 {
     public class Position
     {
+        public char Identifier { get; set; }
         public Coordinates Coordinates { get; set; }
         public CoordinateSystem CoordinateSystem { get; set; }
         public Coordinates ReferenceCoordinates { get; set; }

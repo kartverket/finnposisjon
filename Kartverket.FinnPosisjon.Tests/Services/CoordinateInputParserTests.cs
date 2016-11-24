f5d0a2a70b656f7789141150e34847268ee1c621
Jørgen Tellnes <jorgen@arkitektum.no>
2016-11-24 13:45:28 +0100

    Remove unused reference

--- Kartverket.FinnPosisjon.Tests/Services/CoordinateInputParserTests.cs
+++ Kartverket.FinnPosisjon.Tests/Services/CoordinateInputParserTests.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
 using Kartverket.FinnPosisjon.Services;
 using Xunit;

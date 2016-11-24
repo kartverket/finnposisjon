9fde7eacbd2c71963faf0147debd08ba0598e3ac
Jørgen Tellnes <jorgen@arkitektum.no>
2016-11-24 13:00:40 +0100

    Fix test

--- Kartverket.FinnPosisjon.Tests/Services/CoordinateInputParserTests.cs
+++ Kartverket.FinnPosisjon.Tests/Services/CoordinateInputParserTests.cs
@@ -15,7 +15,6 @@ namespace Kartverket.FinnPosisjon.Tests.Services
             // (1,2) (2,1) (-1,2) (1,-2) (-2,1) (2,-1) Normal/swapped order with/without inverted X/Y
             CoordinateInputParser.GetCoordinates("1", "1", true).Count.Should().Be(6);
 
-            CoordinateInputParser.GetCoordinates("1", "1", "1").Count.Should().Be(1);
         }
     }
 }

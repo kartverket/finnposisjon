6dde57a9422ba5d5d070e748d1504f5d2ea41be2
Jørgen Tellnes <jorgen@arkitektum.no>
2016-11-24 19:35:53 +0100

    Fix test

--- Kartverket.FinnPosisjon.Tests/Services/CoordinateInputParserTests.cs
+++ Kartverket.FinnPosisjon.Tests/Services/CoordinateInputParserTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
+using Kartverket.FinnPosisjon.Models;
 using Kartverket.FinnPosisjon.Services;
 using Xunit;
 
@@ -12,10 +13,10 @@ namespace Kartverket.FinnPosisjon.Tests.Services
         public void GetCoordinatesTest()
         {
             // (1,2) (2,1) Normal/swapped order
-            CoordinateInputParser.GetCoordinates("1", "2").Count.Should().Be(2);
+            CoordinateInputParser.GetCoordinates(new CoordinateInput("1", "2")).Count.Should().Be(2);
 
             // (1,2) (2,1) (-1,2) (1,-2) (-2,1) (2,-1) Normal/swapped order with/without inverted X/Y
-            CoordinateInputParser.GetCoordinates("1", "1", true).Count.Should().Be(6);
+            CoordinateInputParser.GetCoordinates(new CoordinateInput("1", "1"), true).Count.Should().Be(6);
         }
 
         [Fact]

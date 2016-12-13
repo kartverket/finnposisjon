cae84f6c96630254ee7b6688d2895f8dd0bc8262
Benjamin Dehli <benjamin@arkitektum.no>
2016-12-13 16:28:43 +0100

    Adjust script for automatic comprehensive search by URL parameter

--- Kartverket.FinnPosisjon/Content/assets/js/main.js
+++ Kartverket.FinnPosisjon/Content/assets/js/main.js
@@ -272,8 +272,11 @@ var requiredUrlParameters = ['x', 'y'];
 
 $(document).ready(function () {
     var autoTriggerSearch = parametersIsSet(requiredUrlParameters);
-    if (autoTriggerSearch) app.findPositions(false);
-    if (!autoTriggerSearch) {
+    var comprehesiveSearch = getParameterByName('comprehensive') == "true" ? true : false;
+    if (autoTriggerSearch) {
+        app.findPositions(comprehesiveSearch);
+    }
+    else {
         $("#dropdown-container-page-description").addClass("active");
         if (localStorage.getItem("dont-show-description-on-startup") === "false") {
             $("#show-page-description").addClass("active");
--- Kartverket.FinnPosisjon/Content/src/js/init.js
+++ Kartverket.FinnPosisjon/Content/src/js/init.js
@@ -2,8 +2,11 @@
 
 $(document).ready(function () {
     var autoTriggerSearch = parametersIsSet(requiredUrlParameters);
-    if (autoTriggerSearch) app.findPositions(false);
-    if (!autoTriggerSearch) {
+    var comprehesiveSearch = getParameterByName('comprehensive') == "true" ? true : false;
+    if (autoTriggerSearch) {
+        app.findPositions(comprehesiveSearch);
+    }
+    else {
         $("#dropdown-container-page-description").addClass("active");
         if (localStorage.getItem("dont-show-description-on-startup") === "false") {
             $("#show-page-description").addClass("active");

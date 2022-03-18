fa15de5d7f4c217c3f50e242b3d343cab8fe7c15
Jørgen Tellnes <jorgen@tellnes.nu>
2022-03-18 10:16:56 +0100

    Purge old application

--- Kartverket.FinnPosisjon/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kartverket.FinnPosisjon.MvcApplication" Language="C#" %>
--- Kartverket.FinnPosisjon/Models/Coordinates.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Kartverket.FinnPosisjon.Models
-{
-    public class Coordinates
-    {
-        public Coordinate X { get; set; }
-        public Coordinate Y { get; set; }
-    }
-}

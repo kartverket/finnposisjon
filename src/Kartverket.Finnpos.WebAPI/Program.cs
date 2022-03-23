cbc917e0ae4958ad893804bf246f12037580b1b9
Jørgen Tellnes <jorgen@tellnes.nu>
2022-03-23 10:13:20 +0100

    Add empty web API assembly with Swagger

---
+++ src/Kartverket.Finnpos.WebAPI/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.Run();

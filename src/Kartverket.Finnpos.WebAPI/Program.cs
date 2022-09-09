9b84f84d15bd5e1cee7d76f8eb78fb036e96fa2c
Jørgen Tellnes <jorgen@arkitektum.no>
2022-09-09 13:57:47 +0200

    Adjust application name and version appearance

--- src/Kartverket.Finnpos.WebAPI/Program.cs
+++ src/Kartverket.Finnpos.WebAPI/Program.cs
@@ -1,9 +1,13 @@
 using Kartverket.Finnpos.Core;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SwaggerDoc("v1", new OpenApiInfo { Title = "Finnpos", Version = "1.0.0" });
+});
 builder.Services.AddCors();
 
 var app = builder.Build();
@@ -13,7 +17,7 @@ app.UseSwagger(o => o.RouteTemplate = "{documentName}/openapi.json");
 app.UseSwaggerUI(o =>
 {
     o.RoutePrefix = string.Empty;
-    o.SwaggerEndpoint("v1/openapi.json", $"{builder.Environment.ApplicationName} v1");
+    o.SwaggerEndpoint("v1/openapi.json", "Finnpos 1.0.0");
 });
 
 app.MapGet("/positions", Finnpos.GetPositions);
